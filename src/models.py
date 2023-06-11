dc5356300cfce477ba0127e94b59f405f470ddf1
julian19028 <julian_ysv@Icloud.com>
2023-06-11 21:25:47 +0000

    Pending relationships

--- src/models.py
+++ src/models.py
@@ -7,23 +7,47 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Follower(Base):
+    __tablename__ = 'Follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    User_from_id = Column(Integer, primary_key=True)
+    User_to_id = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    iD = Column(Integer, primary_key=True)
+    Username = Column(String(250), nullable=False)
+    Firstname = Column(String(250), nullable=False)
+    Lastname = Column(String(250), nullable=False)
+    Email = Column(String(250), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    iD = Column(Integer, primary_key=True)
+    Comment_text = Column(String(250), nullable=False)
+    Author_ID = Column(Integer, nullable=False)
+    Post_ID = Column(Integer, nullable=False)
+
+class Post(Base):
+    __tablename__ = 'Post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(Integer, primary_key=True)
+    User_id = Column(Integer, nullable=False)  
+
+class Media(Base):
+    __tablename__ = 'Media'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(Integer, primary_key=True)
+    Type = Column(enumerate, primary_key=True)
+    URL = Column(String(250), nullable=False)        
+    Post_ID = Column(Integer, nullable=False) 
 
     def to_dict(self):
         return {}
