8a92828e49ed076161a54b8ede9f31978fdb6dc4
julian19028 <julian_ysv@Icloud.com>
2023-06-12 23:15:47 +0000

    pending

--- src/models.py
+++ src/models.py
@@ -8,46 +8,52 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class Follower(Base):
-    __tablename__ = 'Follower'
+    __tablename__ = 'follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    User_from_id = Column(Integer, primary_key=True)
-    User_to_id = Column(Integer, nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    user_to_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    iD = Column(Integer, primary_key=True)
-    Username = Column(String(250), nullable=False)
-    Firstname = Column(String(250), nullable=False)
-    Lastname = Column(String(250), nullable=False)
-    Email = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False) 
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    follower = relationship ('follower', backref='user', lazy=True)
+    Comment = relationship ('fomment', backref='user', lazy=True)
 
 class Comment(Base):
-    __tablename__ = 'Comment'
+    __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    iD = Column(Integer, primary_key=True)
-    Comment_text = Column(String(250), nullable=False)
-    Author_ID = Column(Integer, nullable=False)
-    Post_ID = Column(Integer, nullable=False)
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    Author_ID = Column(Integer, ForeignKey("user.id"), nullable=False)
+    Post_ID = Column(Integer, ForeignKey("post.id"), nullable=False)
 
 class Post(Base):
-    __tablename__ = 'Post'
+    __tablename__ = 'post'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     ID = Column(Integer, primary_key=True)
-    User_id = Column(Integer, nullable=False)  
+    User_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    Comment = relationship ('comment', backref='post', lazy=True) 
+    Media = relationship ('media', backref='post', lazy=True) 
 
 class Media(Base):
-    __tablename__ = 'Media'
+    __tablename__ = 'media'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     ID = Column(Integer, primary_key=True)
     Type = Column(enumerate, primary_key=True)
     URL = Column(String(250), nullable=False)        
-    Post_ID = Column(Integer, nullable=False) 
+    Post_ID = Column(Integer, ForeignKey("post.id"), nullable=False)
+    
 
     def to_dict(self):
         return {}
